5fd7d0fd373fbcc5881e029e26e0b0c8a22521ac
Taavi Metso <tade.man98@gmail.com>
2017-11-07 21:04:12 +0200

    toinen rikkinäinen versio jossa on yritetty vähän selkeentää koodia

--- src/minerpeli/MapCreator.java
+++ src/minerpeli/MapCreator.java
@@ -23,35 +23,40 @@ public class MapCreator {
     public static void main(String[] args) throws IOException {
         Random r = new Random();
 
-        for (int i = 1; i < 3; i++) {
+        for (int i = 0; i < 3; i++) {
             File file = new File("kartat/kartta" + String.format("%03d", i) + ".dat");
-            MyMap map = new MyMap(30, 10, 5, 5);
-            for (int j = 0; j < map.getWidth(); j++) {
-                for (int k = 0; k < map.getHeight(); k++) {
-                    if (k < 3){
-                        map.setNode(j, k, NodeType.AIR);
-                    }else if (k == 3) {
-                        map.setNode(j, k, NodeType.STONE);
+            MyMap map = new MyMap(30, 40, 5, 5);
+            System.out.println("width: "+Integer.toString(map.getWidth()));
+            System.out.println(map.map.toString());
+            for (int x = 0; x < map.getWidth()-1; x++) {
+                for (int y = 0; y < map.getHeight()-1; y++) {
+                    
+                    if (y < 3){
+                        map.setNode(x, y, NodeType.AIR);
+                    }else if (y == 3) {
+                        map.setNode(x, y, NodeType.STONE);
                     } else if (r.nextFloat() < 0.1) {
-                        map.setNode(j, k, NodeType.HARDSTONE);
+                        map.setNode(x, y, NodeType.HARDSTONE);
                     } else if (r.nextFloat() < 0.1) {
-                        map.setNode(j, k, NodeType.COAL);
+                        map.setNode(x, y, NodeType.COAL);
                     } else if (r.nextFloat() < 0.1) {
-                        map.setNode(j, k, NodeType.IRON);
+                        map.setNode(x, y, NodeType.IRON);
                     } else if (r.nextFloat() < 0.1) {
-                        map.setNode(j, k, NodeType.GOLD);
+                        map.setNode(x, y, NodeType.GOLD);
                     } else if (r.nextFloat() < 0.1) {
-                        map.setNode(j, k, NodeType.AIR);
+                        map.setNode(x, y, NodeType.AIR);
                     } else {
-                        map.setNode(j, k, NodeType.STONE);
+                        map.setNode(x, y, NodeType.STONE);
                     }
                 }
             }
+            map.setNode(20, 2, NodeType.DIAMOND);
             map.setNode(r.nextInt(map.getWidth()-1), map.getHeight()-2-r.nextInt(map.getHeight()/10), NodeType.DIAMOND);
+            /*
             for (int k = 0; k < map.getWidth(); k++){
                 map.setNode(k, map.getHeight()-1, NodeType.HARDSTONE);
             }
-
+            */
             try (FileOutputStream o = new FileOutputStream(file)) {
                 map.writeToStream(o);
 
